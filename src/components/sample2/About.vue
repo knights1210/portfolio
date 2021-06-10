52255f6dd323a2413320324fcd481bc70f69a198
knights1210 <jamis.1984@gmail.com>
2021-06-10 19:11:24 +0900

    SAMPLE2 デザイン修正

--- src/components/sample2/About.vue
+++ src/components/sample2/About.vue
@@ -161,7 +161,10 @@ export default {
         position: absolute;
         content: "";
         background-color: black;
-        inset: 0;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
         opacity: 0;
       }
       &.inview {
